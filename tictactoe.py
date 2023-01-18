e027aae46c1302d8a6d4de49afbe7c349eeed85e
tmwang7324 <tmwang7324@gmail.com>
2023-01-18 18:13:28 -0500

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -36,7 +36,15 @@ def inputO():
         else:
             print('Slot is not within 1-9')
 
-#def win():
+def win():
+    if((board[0] == 'O' and board[1] == 'O' and board[2] == 'O') or (board[0] == 'O' and board[4] == 'O' and board[8] == 'O') or (board[3] == 'O' and board[4] == 'O' and board[5] == 'O') or (board[6] == 'O' and board[7] == 'O' and board[8] == 'O') or (board[0] == 'O' and board[3] == 'O' and board[6] == 'O') or (board[1] == 'O' and board[4] == 'O' and board[7] == 'O') or (board[2] == 'O' and board[5] == 'O' and board[8] == 'O') or (board[2] == 'O' and board[4] == 'O' and board[6] == 'O')):
+        print('O wins')
+    elif((board[0] == 'X' and board[1] == 'X' and board[2] == 'X') or (board[0] == 'X' and board[4] == 'X' and board[8] == 'X') or (board[3] == 'X' and board[4] == 'X' and board[5] == 'X') or (board[6] == 'X' and board[7] == 'X' and board[8] == 'X') or (board[0] == 'X' and board[3] == 'X' and board[6] == 'X') or (board[1] == 'X' and board[4] == 'X' and board[7] == 'X') or (board[2] == 'X' and board[5] == 'X' and board[8] == 'X') or (board[2] == 'X' and board[4] == 'X' and board[6] == 'X')):
+        print('X wins') #"#for x in board &&:)eb7oo winsIe "" or
+    #elif(board[0] == 'O' and board[4] == 'O' and board[8] == 'O'):
+      #  print('O wins')
+    #//elif()        
+        
     
 
 printBoard(board)
