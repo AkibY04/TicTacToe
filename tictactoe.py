3fc227d5f18b5e5e0025be89a211d611ed965cad
AkibY04 <103263291+AkibY04@users.noreply.github.com>
2023-01-18 18:46:18 -0500

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -15,11 +15,14 @@ def inputX():
     while(run):
         inp = input('Enter slot from 1-9 to input X:')
         if(not inp.isdigit()):
-            print('Enter a digit from 1-9 only')
+            print('Enter a digit from 1-9 only:')
         elif(1 <= int(inp) <= 9):
-            print('Success')
-            run = False
-            board[int(inp)-1] = 'X'
+            if(board[int(inp)-1] == 'X' or board[int(inp)-1] == 'O'):
+                print('Current slot is occupied')
+            else:
+                print('Success')
+                run = False
+                board[int(inp)-1] = 'X'
         else:
             print('Slot is not within 1-9')
 
@@ -36,14 +39,12 @@ def inputO():
         else:
             print('Slot is not within 1-9')
 
+#8 ways to win
 def win():
     if((board[0] == 'O' and board[1] == 'O' and board[2] == 'O') or (board[0] == 'O' and board[4] == 'O' and board[8] == 'O') or (board[3] == 'O' and board[4] == 'O' and board[5] == 'O') or (board[6] == 'O' and board[7] == 'O' and board[8] == 'O') or (board[0] == 'O' and board[3] == 'O' and board[6] == 'O') or (board[1] == 'O' and board[4] == 'O' and board[7] == 'O') or (board[2] == 'O' and board[5] == 'O' and board[8] == 'O') or (board[2] == 'O' and board[4] == 'O' and board[6] == 'O')):
         print('O wins')
     elif((board[0] == 'X' and board[1] == 'X' and board[2] == 'X') or (board[0] == 'X' and board[4] == 'X' and board[8] == 'X') or (board[3] == 'X' and board[4] == 'X' and board[5] == 'X') or (board[6] == 'X' and board[7] == 'X' and board[8] == 'X') or (board[0] == 'X' and board[3] == 'X' and board[6] == 'X') or (board[1] == 'X' and board[4] == 'X' and board[7] == 'X') or (board[2] == 'X' and board[5] == 'X' and board[8] == 'X') or (board[2] == 'X' and board[4] == 'X' and board[6] == 'X')):
-        print('X wins') #"#for x in board &&:)eb7oo winsIe "" or
-    #elif(board[0] == 'O' and board[4] == 'O' and board[8] == 'O'):
-      #  print('O wins')
-    #//elif()        
+        print('X wins') 
         
     
 
@@ -51,4 +52,6 @@ printBoard(board)
 inputX()
 printBoard(board)
 inputO()
+printBoard(board)
+inputX()
 printBoard(board)
