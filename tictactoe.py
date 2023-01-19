1ac7ba480307408f083b36cee81cbcbff362ca7b
tmwang7324 <tmwang7324@gmail.com>
2023-01-18 19:24:05 -0500

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,9 +1,16 @@
-board = [
-    "-", "-", "-",
-    "-", "-", "-",
-    "-", "-", "-"
-]
 
+board = [
+        "-", "-", "-",
+        "-", "-", "-",
+        "-", "-", "-"
+    ]
+def clearBoard(board):
+    board = [
+        "-", "-", "-",
+        "-", "-", "-",
+        "-", "-", "-"
+    ]  
+      
 def printBoard(board):
     print(board[0] + ' | ' + board[1] + ' | ' + board[2]) 
     print(board[3] + ' | ' + board[4] + ' | ' + board[5]) 
@@ -33,25 +40,45 @@ def inputO():
         if(not inp.isdigit()):
             print('Enter a digit from 1-9 only')
         elif(1 <= int(inp) <= 9):
-            print('Success')
-            run = False
-            board[int(inp)-1] = 'O'
+        
+            if(board[int(inp)-1] == 'X' or board[int(inp)-1] == 'O'):
+                print('Current slot is occuppied')
+            else:
+                print('Success')
+                run = False
+                board[int(inp)-1] = 'O'
         else:
             print('Slot is not within 1-9')
 
 #8 ways to win
 def win():
+    returnValue = 1
     if((board[0] == 'O' and board[1] == 'O' and board[2] == 'O') or (board[0] == 'O' and board[4] == 'O' and board[8] == 'O') or (board[3] == 'O' and board[4] == 'O' and board[5] == 'O') or (board[6] == 'O' and board[7] == 'O' and board[8] == 'O') or (board[0] == 'O' and board[3] == 'O' and board[6] == 'O') or (board[1] == 'O' and board[4] == 'O' and board[7] == 'O') or (board[2] == 'O' and board[5] == 'O' and board[8] == 'O') or (board[2] == 'O' and board[4] == 'O' and board[6] == 'O')):
-        print('O wins')
+        returnValue = 2
     elif((board[0] == 'X' and board[1] == 'X' and board[2] == 'X') or (board[0] == 'X' and board[4] == 'X' and board[8] == 'X') or (board[3] == 'X' and board[4] == 'X' and board[5] == 'X') or (board[6] == 'X' and board[7] == 'X' and board[8] == 'X') or (board[0] == 'X' and board[3] == 'X' and board[6] == 'X') or (board[1] == 'X' and board[4] == 'X' and board[7] == 'X') or (board[2] == 'X' and board[5] == 'X' and board[8] == 'X') or (board[2] == 'X' and board[4] == 'X' and board[6] == 'X')):
-        print('X wins') 
+        returnValue = 3
+    return returnValue    
+        
+def main():
+    clearBoard(board)
+    printBoard(board)
+    run = True
+    while(run):
+        inputX()
+        printBoard(board)
+        if(win() == 3):
+            break
+        inputO()
+        printBoard(board)
+        if(win() == 2):
+            break
+    if(win() == 2):
+        print('O wins')
+    elif(win() == 3):
+        print('X wins')    
+        
         
     
-
-printBoard(board)
-inputX()
-printBoard(board)
-inputO()
-printBoard(board)
-inputX()
-printBoard(board)
+                
+if __name__ == "__main__":
+    main()
