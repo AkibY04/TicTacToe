8f4149c04e7d7ad88110317ff48d8ab52ac1aab7
Thomas Wang <tmwang7324@gmail.com>
2023-07-05 22:26:34 -0400

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -4,13 +4,16 @@ board = [
     "-", "-", "-"
 ]
 
+player = "X"
+ai = "O" #= ""A###OX
+
 def clearBoard(board):
     board = [
     "-", "-", "-",
     "-", "-", "-",
     "-", "-", "-"
 ]
-#S
+#fS 
 def printBoard(board):
     print(board[0] + ' | ' + board[1] + ' | ' + board[2]) 
     print(board[3] + ' | ' + board[4] + ' | ' + board[5]) 
@@ -50,71 +53,222 @@ def inputO():
         else:
             print('Slot is not within 1-9')
 
+def inputAI():
+    if(ai == "X"):
+        bestScore =-10000 #100000  
+        bestMoveIndex = -10
+        for x in range(9):
+            if(board[x] == '-'):
+                board[x] = ai
+                score = minimax(board, False)
+                
+                board[x] = '-'
+                if(score > bestScore):
+                    bestScore = score
+                    bestMoveIndex = x # I - 1 - 1 - 1 - 1 O 
+        board[bestMoveIndex] = ai
+        print(bestMoveIndex)
+    else:
+        print(2)
+        bestScore = 10000
+        bestMoveIndex = 0
+        for x in range(9):
+            if(board[x] == '-'):
+                board[x] = ai
+                score = minimax(board, True)
+                board[x] = '-'
+                if(score < bestScore):
+                    bestScore = score
+                    bestMoveIndex = x
+        board[bestMoveIndex] = ai
+        print(bestMoveIndex)
+     ##[]scoreE  aiARD=sbcore print(score)0score ai =  player 0score = 0score = 0 in findEmptySpot(board)\xreturn bestMoveIndexbestMove - 1  - 1 - 1 - 1 - 1- 1 - 1 - 1Indexdmaximize()score == 0 in findEmptySpot(board) in findEmptySpot(board)
+    
+def reverseMaximize():
+    
+    bestScore = -10000
+    bestMoveIndex = 0
+    for x in range(9):
+        if(board[x] == '-'):
+                board[x] = player
+                score = minimax(board, False)
+                board[x] = '-'
+                #printBoard(board)
+                if(score > bestScore):
+                    bestScore = score
+                    bestMoveIndex = x
+    return bestScore
+
+def reverseMinimize():
+    
+    bestScore = 10000
+    bestMoveIndex = 0
+    for x in range(9):
+        if(board[x] == '-'):
+            board[x] = ai
+            score = minimax(board, True)
+            board[x] = '-'
+            if(score < bestScore):
+                bestScore = score
+                bestMoveIndex = x
+    return bestScore #mini player
+def maximize():
+    
+    bestScore = -10000
+    bestMoveIndex = 0
+    for x in range(9):
+        if(board[x] == '-'):
+                board[x] = ai
+                score = minimax(board, False)
+                board[x] = '-'
+                #printBoard(board)
+                if(score > bestScore):
+                    bestScore = score
+                    bestMoveIndex = x
+    return bestScore                   
+
+
+def minimize():
+    
+    bestScore = 10000
+    bestMoveIndex = 0
+    for x in range(9):
+        if(board[x] == '-'):
+            board[x] = player
+            score = minimax(board, True)
+            board[x] = '-'
+            if(score < bestScore):
+                bestScore = score
+                bestMoveIndex = x
+    return bestScore
+                        
 def findEmptySpot(board):
     emptySpots = []
     for x in range(9):
         if(board[x] == '-'):
             emptySpots.append(x)
+    #print(emptySpots)o
     return emptySpots    
+    
 
 def calcScore():
-    if(win(board) == 3): 
-        return -5
-    elif(win(board) == 2):
+    if(win() == 3): 
         return 5
-    elif(win(board) == 4):
+    elif(win() == 2):
+        return -5
+    elif(win() == 4):
         return 0
     else:
         return -1
 
-def minimax(board, player): #    C,[i]#if 0emptySpotIndexes[i]indexOfBoardmoves.append( emptySpotIndexes[i]) board[]=ForMovesForMoves ndexndexndexfspoinputO()ts2scor10moves;else: {score: -5} checkAvailability()availreturnreturn -1 -1ning
-    #if(calcScore() != -1):
-        
-    emptySpotIndexes = findEmptySpot(board)
-    score = []
-    moves = []
-    for i in emptySpotIndexes.length:
-        
-        if(player == aiPlayer):
-            board[emptySpotIndexes[i]] = 'O'
-            score.append(calcScore())
-            minimax(board, player)
-    return moves[]    
+scores = {
+    "X" 
+}
+
+def minimax(board, isMaximizing): #    C,[iif(player == "O" and winner = 2):
+        #calcScore];; = player,
+            #if(player == "X")= {}#if  && not 0emptySpotIndexes[i]indexOfBoardmoves.append( emptySpotIndexes[i]) board[]=ForMovesForMoves ndexndexndexfspoinputO()ts2scor10moves;else: {score: -5} checkAvailability()availreturnreturn -1 -1ning
+    #if(calcScore() != -1):9\ !== null 0bestScore = 0  cal == win(board)bboardoardboardint()type(score)score = 0sscalcScore()#
     
-#def max(board) #avail
+    winner = calcScore()
+    #
+    if(winner != -1):
+        score = calcScore()
+        print(score)
+        return score
+    if(ai == "X"):        
+        if(isMaximizing):
+            bestScore = maximize()
+        else: 
+            bestScore = minimize()
+        return bestScore
+    else:
+        if(isMaximizing):
+            bestScore = reverseMaximize()
+        else: #;    
+            bestScore = reverseMinimize()
+        return bestScore    
 
+    
 
-#8 ways to win
-def win(board):
+#8 ways to winboardboardgth.Player vs AI
+def win():
     returnValue = 1
     if((board[0] == 'O' and board[1] == 'O' and board[2] == 'O') or (board[0] == 'O' and board[4] == 'O' and board[8] == 'O') or (board[3] == 'O' and board[4] == 'O' and board[5] == 'O') or (board[6] == 'O' and board[7] == 'O' and board[8] == 'O') or (board[0] == 'O' and board[3] == 'O' and board[6] == 'O') or (board[1] == 'O' and board[4] == 'O' and board[7] == 'O') or (board[2] == 'O' and board[5] == 'O' and board[8] == 'O') or (board[2] == 'O' and board[4] == 'O' and board[6] == 'O')):
         returnValue = 2
     elif((board[0] == 'X' and board[1] == 'X' and board[2] == 'X') or (board[0] == 'X' and board[4] == 'X' and board[8] == 'X') or (board[3] == 'X' and board[4] == 'X' and board[5] == 'X') or (board[6] == 'X' and board[7] == 'X' and board[8] == 'X') or (board[0] == 'X' and board[3] == 'X' and board[6] == 'X') or (board[1] == 'X' and board[4] == 'X' and board[7] == 'X') or (board[2] == 'X' and board[5] == 'X' and board[8] == 'X') or (board[2] == 'X' and board[4] == 'X' and board[6] == 'X')):
         returnValue = 3
-    elif(findEmptySpot(board).length == 0):
+    elif(len(findEmptySpot(board)) == 0):
         returnValue = 4     
     return returnValue    
         
 def main():
     clearBoard(board)
     printBoard(board)
-    run = True
-    while(run):
-        inputX()
-        printBoard(board)
-        if(win() == 3 or win() == 4):
-            break
-        inputO()
-        printBoard(board)
-        if(win() == 2 or win() == 4):
-            break
-        
-    if(win() == 2):
-        print('O wins')
-    elif(win() == 3):
-        print('X wins')    
-    elif(win() == 4):
-        print('Its a draw!')
+    inp = input("Player vs AI or Player vs Player")
+    if(inp == "1"):
+        inp2 = input("Do you want to be X or O?")
+        if(inp2 == "X"):
+            player = "X"
+            ai = 'O' #@""
+        elif(inp2 == "O"):
+            ai = "X"
+            player = "O"    
+            
+        else:
+            print("Enter X or O")
+        run = True
+        while(run):
+            if(player == "X"):
+                inputX()  
+                printBoard(board)
+                if(win() == 3 or win() == 4):
+                  break
+                inputAI()
+                printBoard(board)
+                if(win() == 2 or win() == 4):
+                    break 
+                if(win() == 2):
+                    print('O wins')
+                elif(win() == 3):
+                    print('X wins')    
+                elif(win() == 4):
+                    print('Its a draw!')
+            elif(player == "O"):
+                inputAI()
+                printBoard(board)
+                if(win() == 3 or win() == 4):
+                  break
+                inputO()
+                printBoard(board)
+                if(win() == 2 or win() == 4):
+                    break 
+                if(win() == 2):
+                    print('O wins')
+                elif(win() == 3):
+                    print('X wins')    
+                elif(win() == 4):
+                    print('Its a draw!')
+                
+                     #if(win())         
+    else:                
+        run = True
+        while(run):
+            inputX()
+            printBoard(board)
+            if(win() == 3 or win() == 4):
+                break
+            inputO()
+            printBoard(board)
+            if(win() == 2 or win() == 4):
+                break
+            
+        if(win() == 2):
+            print('O wins')
+        elif(win() == 3):
+            print('X wins')    
+        elif(win() == 4):
+            print('Its a draw!')
                 
 if __name__ == "__main__":
     main()
